dc42e0ee835f873eae9e98787c72e81e80279b9a
ArnaudLacroix <arnaud.lacroix.pro@gmail.com>
2021-11-05 16:01:35 +0100

    fixed design of the cart

--- src/App.js
+++ src/App.js
@@ -27,6 +27,28 @@ function App() {
     fetchData();
   }, []);
 
+  const minusIcon = (
+    <svg
+      height="18"
+      width="18"
+      viewBox="0 0 24 24"
+      className="add-remove-icons"
+    >
+      <path d="M12 2C17.5228 2 22 6.47725 22 12C22 17.5228 17.5228 22 12 22C6.47717 22 2 17.5228 2 12C2 6.47725 6.47717 2 12 2ZM12 20C16.4113 20 20 16.4113 20 12C20 7.58875 16.4113 4 12 4C7.58875 4 4 7.58875 4 12C4 16.4113 7.58875 20 12 20ZM7 13.5V10.5H17V13.5H7Z"></path>
+    </svg>
+  );
+
+  const plusIcon = (
+    <svg
+      height="18"
+      width="18"
+      viewBox="0 0 24 24"
+      className="add-remove-icons"
+    >
+      <path d="M12 2C17.5228 2 22 6.47725 22 12C22 17.5228 17.5228 22 12 22C6.47717 22 2 17.5228 2 12C2 6.47725 6.47717 2 12 2ZM12 20C16.4113 20 20 16.4113 20 12C20 7.58875 16.4113 4 12 4C7.58875 4 4 7.58875 4 12C4 16.4113 7.58875 20 12 20ZM13.5 7V10.4999H17V13.5H13.5V17H10.5V13.5H7V10.4999H10.5V7H13.5Z"></path>
+    </svg>
+  );
+
   return isLoading ? (
     <span>En cours de chargement</span>
   ) : (
@@ -86,7 +108,11 @@ function App() {
                                   <p>{meal.price.replace(".", ",")} €</p>
                                   {meal.popular && (
                                     <span className="popularity">
-                                      <img className="star" src={popularStar} />
+                                      <img
+                                        className="star"
+                                        src={popularStar}
+                                        alt="Favorite icon"
+                                      />
                                       Populaire
                                     </span>
                                   )}
@@ -123,8 +149,18 @@ function App() {
             )}
             <div className="cart-list">
               {cart.length > 0 ? (
-                cart.map((item, index) => {
-                  return <div key={index}>{item.title}</div>;
+                cart.map((product, index) => {
+                  return (
+                    <div className="cart-item">
+                      <div className="cart-counter">
+                        <button>{minusIcon}</button>
+                        <span>1</span>
+                        <button>{plusIcon}</button>
+                      </div>
+                      <span className="cart-product">{product.title}</span>
+                      <span className="cart-amount">{product.price} €</span>
+                    </div>
+                  );
                 })
               ) : (
                 <p className="empty-cart">Votre panier est vide</p>
